99f26eb16b3b9a4aa86a14589230fab2d1bdc117
TranTam <tamvaa1306@gmail.com>
2023-04-26 10:21:49 +0700

    update tailwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: ["./**/*.html", "./**/*.{js,jsx,ts,tsx,vue}"],
-
+  purge: ["./*.html"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
         35: "35px",
       },
       colors: {
-        gray33: "#333333",
-        orangefa: "#FFA400",
+        gray33: "#333",
+        orangefa: "#ffa400",
       },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["odd"],
     },
   },
   plugins: [],
